cfec0c6d1f97adb3bdad3923bffdbed7871fd5b8
SFhectorj <SFhectorj@icloud.com>
2024-12-03 19:59:03 -0800

    hw9

--- TSP.py
+++ TSP.py
@@ -8,4 +8,10 @@ def solve_tsp(G):
     cities_already_visited =
     travel_path = []
     current_city = 0 # Starts from the first node
+
+    # Begin with starting city
+    travel_path.append(current_city)
+    cities_already_visited[current_city] = True
+
+
     return

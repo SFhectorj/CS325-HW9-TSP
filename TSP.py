bb0536c05d78d98aefc3962b24113f2fde338409
SFhectorj <SFhectorj@icloud.com>
2024-12-03 19:46:48 -0800

    hw9

--- TSP.py
+++ TSP.py
@@ -1,2 +1,10 @@
 def solve_tsp(G):
+    """
+    This function is a greedy implementation of the traveling salesperson problem where
+    the next city to visit is always the nearest unvisited neighbor.
+    """
+    # Initialize
+    number_of_cities = len(G)
+    cities_already_visited =
+    travel_path = []
     return
